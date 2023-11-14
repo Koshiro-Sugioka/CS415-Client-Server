4e7775ca9a46befcfe833a5ef6d313695cf7423b
Koshiro Sugioka <kousirou331@gmail.com>
2023-11-14 07:23:45 +0000

    Add client server program

---
+++ server.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    // Bind the socket to the network address and port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for client connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept a connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        //reading the message from client
+        int read_val = read(new_socket, buffer, 1024);
+        if (read_val <= 0) {
+            break;
+        }
+
+        std::cout << "Client: " << buffer << std::endl;
+        std::cout << "Server: ";
+        std::string server_message;
+        std::getline(std::cin, server_message);
+        //sending the message to client
+        send(new_socket, server_message.c_str(), server_message.size(), 0);
+    }
+
+    close(server_fd);
+    return 0;
+}
